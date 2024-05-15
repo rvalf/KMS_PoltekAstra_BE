365a9894374617b3cb6eb0860c5f14ab852c6efa
Ffathiyl <m.fatihalfalahi2003@gmail.com>
2024-05-15 13:24:48 +0700

    Merge BE from Fatih

--- src/main/java/com/example/kms_poltekastra_be/rest/UttlitiesRest.java
+++ src/main/java/com/example/kms_poltekastra_be/rest/UttlitiesRest.java
@@ -4,6 +4,7 @@ package com.example.kms_poltekastra_be.rest;
 import com.example.kms_poltekastra_be.service.UtilitiesService;
 import com.example.kms_poltekastra_be.service.impl.UtilitiesServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -37,5 +38,10 @@ public class UttlitiesRest {
     @PostMapping("/Upload")
     public ResponseEntity<?> UploadFile (@RequestBody MultipartFile file){
         return utilitiesService.uploadFile(file);
+    }
+
+    @GetMapping("/Upload/DownloadFile")
+    public ResponseEntity<Resource> downloadFile(@RequestParam String namaFile) {
+        return utilitiesService.downloadFile(namaFile);
     }
 }
