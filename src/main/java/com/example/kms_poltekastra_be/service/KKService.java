873fcef3723c43b930913aacb4634b8d28182dc9
rvalf <revalinaf180@gmail.com>
2024-05-15 12:33:22 +0700

    getlistprodi, getlistkaryawan, createKK, getKKbyID

--- src/main/java/com/example/kms_poltekastra_be/service/KKService.java
+++ src/main/java/com/example/kms_poltekastra_be/service/KKService.java
@@ -3,9 +3,9 @@ package com.example.kms_poltekastra_be.service;
 import java.util.Map;
 
 public interface KKService {
-    public String getDataKK(Map<String, Object> data);
-    public String getDataKKbyID(Map<String, Object> data);
-    public String getListProdi(Map<String, Object> data);
-    public String getListKaryawan(Map<String, Object> data);
-    public String createKK(Map<String, Object> data);
+    String getDataKK(Map<String, Object> data);
+    String getDataKKbyID(Map<String, Object> data);
+    String getListProdi(Map<String, Object> data);
+    String getListKaryawan(Map<String, Object> data);
+    String createKK(Map<String, Object> data);
 }
